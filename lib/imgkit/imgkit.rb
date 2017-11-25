a852b0801140991c0d52e5e00662dda3cb53a07c
Nguyen Truong Sinh <ntsinh90@gmail.com>
2017-11-25 16:07:28 +0700

    Update imgkit.rb

--- lib/imgkit/imgkit.rb
+++ lib/imgkit/imgkit.rb
@@ -113,7 +113,7 @@ class IMGKit
 
     opts = @source.html? ? {:stdin_data => @source.to_s} : {}
     result, stderr = capture3(*(command(path) + [opts]))
-    result.force_encoding("ASCII-8BIT") if result.respond_to? :force_encoding
+    # result.force_encoding("ASCII-8BIT") if result.respond_to? :force_encoding
     raise CommandFailedError.new(command.join(' '), stderr) if path.nil? and result.size == 0
     result
   end
